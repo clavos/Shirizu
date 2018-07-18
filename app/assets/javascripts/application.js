08f91e2bbbe97f4349fce93d78cee21a232f8462
roche <roche@roche-XPS-13-1943.com>
2018-07-18 11:15:29 +0200

    ajouter home page style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,13 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
+
+  $(function(){
+$("#addClass").click(function () {
+          $('#qnimate').addClass('popup-box-on');
+            });
+          
+            $("#removeClass").click(function () {
+          $('#qnimate').removeClass('popup-box-on');
+            });
+  })
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_17_193716) do
+ActiveRecord::Schema.define(version: 2018_07_13_221638) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -40,10 +40,6 @@ ActiveRecord::Schema.define(version: 2018_07_17_193716) do
     t.integer "season_id"
     t.string "image"
     t.index ["season_id"], name: "index_episodes_on_season_id"
-  end
-
-  create_table "lists", force: :cascade do |t|
-    t.string "name"
   end
 
   create_table "seasons", force: :cascade do |t|
@@ -86,7 +82,6 @@ ActiveRecord::Schema.define(version: 2018_07_17_193716) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
